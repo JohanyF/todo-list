f3a0d0c63016776b3732efdb6ac121a9738a76e5
JohanyF <johanyf627@gmail.com>
2024-07-27 02:20:04 -0500

    Created Project Class

---
+++ src/project.js
@@ -0,0 +1,45 @@
+
+import Task from './task.js';
+
+export default class Project {
+    #tasks = []
+    constructor(name) {
+        this._name = name;
+    }
+
+    get tasks() {
+        return this.#tasks;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+
+    addTask(title, description, date, priority, project) {
+        let task = new Task(title, description, date, priority, project);
+        console.log(task);
+        this.#tasks.push(task);
+        console.log(this.#tasks);
+    }
+
+    printTasks() {
+        this.#tasks.forEach((task) => {
+            console.log(`${task.title} | ${task.description} | ${task.date} | ${task.priority} | ${task.project}`);
+        })
+    }
+
+
+    // edit() {
+
+    // }
+
+    // delete() {
+
+    // }
+
+
+
+    // I think project class will include what the functionality of the Inbox, Today, and Upcoming tabs will do... Might have to make it its own file where you import this class???
+    // Note: This will not involve any code that makes changes to the DOM that will be in another file that should do that
+}
