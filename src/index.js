ee462462d9b7dd6c3cc504212fb8712f554b0174
JohanyF <johanyf627@gmail.com>
2024-07-27 02:21:59 -0500

    Added functionality between the DOM and the logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 import './style.css';
 
-console.log("TEST");
+import Project from './project.js';
+import DOMHandler from './DOMHandler.js';
+
+let DOMhander = new DOMHandler();
+
+DOMhander.loadSVGs();
+
+let inboxTest = new Project("Inbox");
+
+const addBtn = DOMhander.addBtn;
+const cancelBtn = DOMhander.cancelBtn;
+const submitBtn = DOMhander.submitBtn;
+
+console.log(addBtn);
+console.log(cancelBtn);
+console.log(submitBtn);
+
+addBtn.addEventListener("click", () => {
+    modal.showModal();
+})
+
+cancelBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    modal.close();
+    document.taskForm.reset();
+})
+
+submitBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log("BTN WORKKSSS")
+    inboxTest.addTask(DOMhander.taskTitle.value, DOMhander.description.value, DOMhander.date.value, DOMhander.priority.value, DOMhander.projects.value);
+    inboxTest.printTasks();
+    DOMhander.renderNewTask(inboxTest.tasks.length-1);
+    modal.close();
+    document.taskForm.reset();
+})
