9b57cd1959f1a97deac2dba7c4b52bbbc4b70c2d
JohanyF <johanyf627@gmail.com>
2024-07-27 02:20:24 -0500

    Created Task Class

---
+++ src/task.js
@@ -0,0 +1,10 @@
+
+export default class Task {
+    constructor(title, description, date, priority, project) {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.priority = priority;
+        this.project = project;
+    }
+}
