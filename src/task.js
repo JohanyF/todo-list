c7464f7f26bb0964eef6acc740c2201b1c9f67ae
JohanyF <johanyf627@gmail.com>
2024-08-07 02:14:47 -0500

    Created setter methods for task.js

--- src/task.js
+++ src/task.js
@@ -1,10 +1,47 @@
 
 export default class Task {
     constructor(title, description, date, priority, project) {
-        this.title = title;
-        this.description = description;
-        this.date = date;
-        this.priority = priority;
-        this.project = project;
+        this._title = title;
+        this._description = description;
+        this._date = date;
+        this._priority = priority;
+        this._project = project;
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    get description() {
+        return this._description;
+    }
+
+    get date() {
+        return this._date;
+    }
+
+    get priority() {
+        return this._priority;
+    }
+
+    get project() {
+        return this._project;
+    }
+
+    set title(title) {
+        this._title = title;
+    }
+
+    set description(description) {
+        this._description = description;
+    }
+    set date(date) {
+        this._date = date;
+    }
+    set priority(priority) {
+        this._priority = priority;
+    }
+    set project(project) {
+        this._project = project;
     }
 }
